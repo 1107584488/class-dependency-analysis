afc1023e0d662e03090a4ff65b665a0154516ab4
lianguowei <lianguowei@kuaishou.com>
2024-09-03 16:14:23 +0800

    数据屏蔽

--- class-dependency-analysis-local/src/main/java/com/lgw/github/Main.java
+++ class-dependency-analysis-local/src/main/java/com/lgw/github/Main.java
@@ -21,11 +21,20 @@ public class Main {
         // 本地excel文件名，不带.xlsx后缀，sheet仅一列，同名
         String excelFileName = "CreativeDomain";
         // 域命名规范前缀
-        String moduleRule = "com.kuaishou.ad.brand.platform";
+        String moduleRule = "com.xxx.ad.brand.platform";
         // 待扫描输出excel内容的 扫描类文件路径
         List<String> scanPathList = Arrays.asList(
-                "/Users/lianguowei/Documents/soft/ideaProject/ad-brand-platform/ad-brand-platform-api/src/main/java/com/ad/brand/platform/controller/creative/CreativeController.java"
-                , "/Users/lianguowei/Documents/soft/ideaProject/ad-brand-platform/ad-brand-platform-api/src/main/java/com/ad/brand/platform/controller/AppController.java"
+                "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/creative/CreativeController.java"
+                , "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/gray/UnitAndCreativeGrayController.java"
+                , "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/tube/TubeController.java"
+                , "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/unit/UnitController.java"
+                , "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/AdBrandCampaignController.java"
+                , "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/AdBrandCommentController.java"
+                , "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/AdBrandCreativeController.java"
+                , "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/AdBrandPhotoController.java"
+                , "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/AdBrandTargetController.java"
+                , "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/AdBrandUnitController.java"
+                , "/Users/lianguowei/Documents/soft/ideaProject/xxx-ad-brand-platform/xxx-ad-brand-platform-api/src/main/java/com/xxx/ad/brand/platform/controller/AppController.java"
         );
 
         // 开始执行前删除原有的excel文件
