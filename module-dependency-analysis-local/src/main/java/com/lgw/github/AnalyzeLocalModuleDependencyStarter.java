6b01fb7856c9a46d8c97e9f403a5ab09e4960eea
lianguowei <lianguowei@kuaishou.com>
2024-12-16 20:41:12 +0800

    数据脱敏、执行前删除本地原有文件

--- module-dependency-analysis-local/src/main/java/com/lgw/github/AnalyzeLocalModuleDependencyStarter.java
+++ module-dependency-analysis-local/src/main/java/com/lgw/github/AnalyzeLocalModuleDependencyStarter.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 import com.lgw.github.constant.Variable;
 import com.lgw.github.util.ExcelUtil;
+import com.lgw.github.util.FileUtil;
 import com.lgw.github.util.PrintUtil;
 import com.lgw.github.util.ScanUtil;
 
@@ -20,28 +21,37 @@ public class AnalyzeLocalModuleDependencyStarter {
     public static void main(String[] args) {
         // 键入信息
         // 本地excel输出路径
-        String outFilePath = "/Users/lianguowei/Documents/草稿/依赖扫描";
+        String outFilePath = "/Users/lianguowei/Documents/草稿/依赖扫描2";
         // 本地excel文件名，不带.xlsx后缀，sheet仅一列，同名
-        String excelFileName = "ShareSDK";
+        String excelFileName = "CreativeSDK";
+
+        // 开始执行前删除原有的excel文件
+        FileUtil.deleteFile(outFilePath, excelFileName);
 
         // 步骤1:输入待扫描的模块和路径（即需要扫描的被依赖的模块），自动化嵌套扫描类全路径
         // key - 模块名
         // value - 扫描的起始包路径
         Map<String, String> dependencyModuleMap = new LinkedHashMap<>() {{
-            put("Settle", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-settle-sdk");
-            put("Common", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-common-sdk");
-            put("Order", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-order-sdk");
-            put("Account", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-account-sdk");
-            put("Resource", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-resource-sdk");
-            put("Creative", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-creative-sdk");
+            put("ShareSDK", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-share-sdk");
+            put("CommonSDK", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-common-sdk");
+            put("SettleSDK", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-settle-sdk");
+            put("OrderSDK", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-order-sdk");
+            put("AccountSDK", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-account-sdk");
+            put("ResourceSDK", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-resource-sdk");
+
+            put("ShareComponent", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-share-component");
+            put("CommonComponent", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-common-component");
+            put("SettleComponent", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-settle-component");
+            put("OrderComponent", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-order-component");
+            put("AccountComponent", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-account-component");
+            put("ResourceComponent", "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-resource-component");
         }};
         // 扫描出当前模块下的所有类路径
         scanDependencyModuleClassPath(dependencyModuleMap);
 
-
         // 步骤2:输入待扫描的模块和路径（即需要分析的模块），自动化嵌套扫描类全路径
-        String scanModuleName = "ShareSDK";
-        String modulePath = "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-share-sdk";
+        String scanModuleName = "CreativeSDK";
+        String modulePath = "/Users/lianguowei/Documents/soft/ideaProject/依赖分析/xxx-ad-brand-platform/ad-brand-creative-sdk";
         Map<String, String> fileNamePathMap = getScanFolderClassName(modulePath);
         if (!fileNamePathMap.isEmpty()) {
             for (Map.Entry<String, String> fileNamePathMapEntry : fileNamePathMap.entrySet()) {
